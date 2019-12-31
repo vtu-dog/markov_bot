135ab2054c0173cf8d58b6eb66f19cee242430a9
Wojciech Jarząbek <WojciechJarzabek@windowslive.com>
2019-12-31 06:59:21 +0100

    Uncluttered logs

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,8 @@ async fn main () {
 
     let chain = Arc::new(Mutex::new(chain_wrapper::ChainWrapper::new()));
     let bot = bot::create(chain.clone());
-    let polling = bot.polling().start();
+
+    let polling = bot.polling().error_handler(|_| async move { }).start();
 
     select(Box::pin(polling), Box::pin(sig)).await;
     chain.lock().unwrap().drop_all();
